e7cfa1d93df0a64bf7a2c7b6922affc57bcaad8f
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2023-11-22 17:22:11 +0800

    added new cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const cookieSession = require("cookie-session");
 const dbConfig = require("./app/config/db.config");
 
+const app = express();
 app.use(function(req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   const allowedOrigins = ["http://192.168.1.178:8081", 
@@ -23,7 +24,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-const app = express();
+
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
