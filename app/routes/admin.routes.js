4f9a8faebb41ae5313f6c5bfe7a9dfd7a3b01fac
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2023-11-23 18:11:10 +0800

    test cors admin

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -3,7 +3,10 @@ const controller = require("../controllers/admin.controller");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    res.header(
+      "Access-Control-Allow-Headers: *", 
+      "Origin, Content-Type, Accept"
+      );
     next();
   });
 
