044c195722a0edde28507da4ff4b087890a15caf
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2023-11-25 09:48:28 +0800

    TEST env 2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 module.exports = {
-    url: "mongodb+srv://angelonatad22:j2xroMaxp8cSfQEr@dormate.u7iebjw.mongodb.net/",
+    url: process.env.MONGO_URL,
     HOST: "0.0.0.0",
-    PORT: 27017,
-    database: "dormate_db",
+    PORT: process.env.PORT,
+    database: process.env.DATABASE,
     imgBucketUser: "user_photos",
     imgBucketDorm: "dorm_photos",
     imgBucketCertificate: "certificate_photos",
-    DB: "dormate_db",
-    PASS: "j2xroMaxp8cSfQEr"
+    DB: process.env.DATABASE,
+    PASS: process.env.PASS
   };
 
   //DB HOST: "0.0.0.0", 
