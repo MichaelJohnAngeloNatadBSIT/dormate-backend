87a3a3ed1525affbbd511057bb4bcbab47c82294
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2023-11-25 09:44:24 +0800

    reverted

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    url: process.env.MONGO_URL,
+    url: "mongodb+srv://angelonatad22:j2xroMaxp8cSfQEr@dormate.u7iebjw.mongodb.net/",
     HOST: "0.0.0.0",
-    PORT: process.env.PORT,
+    PORT: 27017,
     database: "dormate_db",
     imgBucketUser: "user_photos",
     imgBucketDorm: "dorm_photos",
     imgBucketCertificate: "certificate_photos",
     DB: "dormate_db",
-    PASS: process.env.PASS
+    PASS: "j2xroMaxp8cSfQEr"
   };
 
   //DB HOST: "0.0.0.0", 
