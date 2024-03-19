11865132145c3fa52c1490aca37d2e2ca791feb6
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2024-03-19 19:24:16 +0800

    reverted

--- app/controllers/dorm.controller.js
+++ app/controllers/dorm.controller.js
@@ -591,7 +591,7 @@ exports.findAll = (req, res) => {
   };
   const title = req.query.title;
 
-  Dorm.find({ $or: [ { title: {$regex: new RegExp(title), $options: "i" } }, { address: {$regex: new RegExp(title), $options: "i" } } ] }, condition)
+  Dorm.find({ $or: [ { title: {$regex: new RegExp(title), $options: "i" } }, { address: {$regex: new RegExp(title), $options: "i" } } ] })
     .then((data) => {
       res.send(data);
     })
