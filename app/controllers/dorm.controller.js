7fcac1d9ae68687b575d14e1e8640c8488ea3a39
MichaelJohnAngeloNatadBSIT <201911707@gordoncollege.edu.ph>
2024-05-12 14:38:21 +0800

    checking values for evict

--- app/controllers/dorm.controller.js
+++ app/controllers/dorm.controller.js
@@ -183,7 +183,7 @@ exports.evictTenant = async (req, res) => {
     const id = req.params.id;
     const newTenant = req.body;
 
-    console.log(newTenant);
+    console.log(newTenant, id);
 
     // Find the dormitory by ID
     const dorm = await Dorm.findById(id);
